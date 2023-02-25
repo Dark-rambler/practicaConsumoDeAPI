9411a72dd10859ed21c172e2e1f6d6e6794a3fbc
Dark-rambler <80594417+Dark-rambler@users.noreply.github.com>
2023-02-25 11:00:46 -0400

    inicio responsive

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -9,7 +9,7 @@ function Inicio(){
     return(
         <div className="text-primary principal  container">
 
-                <article className="description d-flex ">
+                <article className="description d-flex ms-4">
                     <Fondo/>
                     
                     <div className="palabra row  rounded">
@@ -21,12 +21,12 @@ function Inicio(){
                             <p className=" pt-2 ms-4 pb-3 mb-5 ">
                                 Encuentra estadistica e informacion para ser el proximo maestro pokemon!
                              </p> 
-                             <div>
-                            <Link to={"/todos"} type="button" class="btn btn-secondary w-25 ms-5"> Ingresar </Link>
-                            <Link to={"/informacion"} type="button" class="btn btn-outline-secondary w-25 ms-5"> Informacion </Link>
+                             <div className="row">
+                            <Link to={"/todos"} type="button" class="btn btn-secondary  ms-5 col-8"> Ingresar </Link>
+                            <Link to={"/informacion"} type="button" class="btn btn-outline-secondary mt-3 ms-5 col-8 "> Informacion </Link>
                             </div>
                     </section>
-                    <section className="w-50 col col-sm-12  ">
+                    <section className="w-50 col col-sm-12 d-none d-md-inline ">
                     <img src="https://raw.githubusercontent.com/Dark-rambler/practicaConsumoDeAPI/main/pngwing.com.png" className="  img-fluid" width={"500px"} alt="" />
                     </section>
                     </div>
